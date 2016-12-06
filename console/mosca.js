f5f2ba535ec0eb494cfa1087f1c6ff337419adf0
Simone Cilli <simone.cilli.84@datalogic.com>
2016-12-06 22:12:58 +0100

    Add AJAX request/response structure in console

--- console/mosca.js
+++ console/mosca.js
@@ -1,18 +1,23 @@
 var mosca = require('mosca');
-
+var http = require("http");
+ 
 // CONSTANTS
-var httpPort = 9001;
-var ajaxPort = 9002;
-var mqttPort = 1883;
+var httpPort   = 9001;
+var mqttPort   = 1883;
+var ajaxPort   = 9002;
+var ajaxPollMs = 1000;
+var ajaxToMs   = 60000;
 var wwwStaticFolder = './www/';
 
 // SHOW CONFIGURATION
 console.log("");
 console.log("HTTP PORT : " + httpPort);
+console.log("AJAX PORT : " + ajaxPort);
 console.log("MQTT PORT : " + mqttPort);
 console.log("WWW DIR.  : " + wwwStaticFolder);
 console.log("");
 
+// MQTT
 var settings = {
   port: mqttPort,
   http: {
@@ -20,66 +25,37 @@ var settings = {
     bundle: true,
     static: wwwStaticFolder
   }
-  //backend: ascoltatore
 };
-
 var server = new mosca.Server(settings);
 
 server.on('clientConnected',     function(client) { console.log('client connected', client.id); });
 server.on('clientDisconnecting', function(client) { console.log('clientDisconnecting : ', client.id); });
 server.on('clientDisconnected',  function(client) { console.log('clientDisconnected : ', client.id); });
-
 server.on('published',    function(packet, client) { console.log('Published', packet.payload); });
 server.on('subscribed',   function(topic, client) { console.log('subscribed : ', topic); });
 server.on('unsubscribed', function(topic, client) { console.log('unsubscribed : ', topic); });
+server.on('ready', function() { console.log('Mosca server is up and running'); });
 
-server.on('ready', setup);
+// AJAX
+http.createServer(function(request, response) {
+	var requestDate = new Date();
+	checkIOTStatus(request, requestDate, response);
+}).listen(ajaxPort);
 
-// fired when the mqtt server is ready
-function setup() {
-  console.log('Mosca server is up and running');
+function checkIOTStatus(request, requestDate, response)
+{
+	var date = new Date();
+	if (date-requestDate >= ajaxToMs) {
+		response.writeHead(200, {
+			'Content-Type'   : 'text/plain',
+			'Access-Control-Allow-Origin' : '*'
+		});
+		response.write('OK', 'utf8');
+		response.end();
+		return false;
+	}
+ 
+	//TODO: iot logic
+ 
+	setTimeout(function() { checkIOTStatus(request, requestDate, response) }, ajaxPollMs);
 }
-
-//To see for other functions
-/*
-server.on('ready', setup);
-
-// fired when the mqtt server is ready
-function setup() {
-  console.log('Mosca server is up and running')
-}
-
-// fired whena  client is connected
-server.on('clientConnected', function(client) {
-  console.log('client connected', client.id);
-});
-
-// fired when a message is received
-server.on('published', function(packet, client) {
-  if (packet.cmd === 'publish') {
-    //Qui uso mongo DB 
-    console.log('Published: ', packet.payload.toString('utf8'));
-  }
-});
-
-// fired when a client subscribes to a topic
-server.on('subscribed', function(topic, client) {
-  console.log('subscribed : ', topic);
-});
-
-// fired when a client subscribes to a topic
-server.on('unsubscribed', function(topic, client) {
-  console.log('unsubscribed : ', topic);
-});
-
-// fired when a client is disconnecting
-server.on('clientDisconnecting', function(client) {
-  console.log('clientDisconnecting : ', client.id);
-});
-
-// fired when a client is disconnected
-server.on('clientDisconnected', function(client) {
-  console.log('clientDisconnected : ', client.id);
-});
-client.html
-*/
